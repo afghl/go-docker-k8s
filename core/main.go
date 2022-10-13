d5cd48c6cedec8d2792643d9542e28b9c943d6dc
airport1030 <airport.0930@gmail.com>
2022-10-13 15:57:33 +0800

    use docker file to build image and run container

---
+++ core/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
+
+func reportIP(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "reportIP, my ip: %v.\n", getLocalIP())
+}
+
+func main() {
+	http.HandleFunc("/reportip", reportIP)
+	http.ListenAndServe(":8090", nil)
+}
+
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
